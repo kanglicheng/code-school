8b23ddf5e9dc53a1baf30982df8158ac74323b14
Debra Fong <debrafong@Debras-MacBook-Air.local>
2017-01-13 00:24:45 -0800

    setup modal-lightning-talk

--- react/projects/bench_bnb/solution/frontend/components/greeting/greeting.jsx
+++ react/projects/bench_bnb/solution/frontend/components/greeting/greeting.jsx
@@ -1,23 +1,40 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-const sessionLinks = () => (
-  <nav className="login-signup">
-    <Link to="/login" activeClassName="current">Login</Link>
-    &nbsp;or&nbsp;
-    <Link to="/signup" activeClassName="current">Sign up!</Link>
-  </nav>
-);
+class Greeting extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-const personalGreeting = (currentUser, logout) => (
-	<hgroup className="header-group">
-    <h2 className="header-name">Hi, {currentUser.username}!</h2>
-    <button className="header-button" onClick={logout}>Log Out</button>
-	</hgroup>
-);
+  sessionLinks() {
+    return(
+      <nav className="login-signup">
+        <Link to="/login" activeClassName="current">Login</Link>
+        &nbsp;or&nbsp;
+        <Link to="/signup" activeClassName="current">Sign up!</Link>
+      </nav>
+    );
+  }
 
-const Greeting = ({ currentUser, logout }) => (
-  currentUser ? personalGreeting(currentUser, logout) : sessionLinks()
-);
+  personalGreeting(currentUser, logout) {
+    return(
+      <hgroup className="header-group">
+        <h2 className="header-name">Hi, {currentUser.username}!</h2>
+        <button className="header-button" onClick={logout}>Log Out</button>
+      </hgroup>
+    );
+  }
+
+  render() {
+    return(
+      <div>
+        {this.props.currentUser ? this.personalGreeting(this.props.currentUser, this.props.logout) : this.sessionLinks()}
+      </div>
+    );
+  }
+}
+
+
+
 
 export default Greeting;
