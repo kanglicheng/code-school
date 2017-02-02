20dda18e28af43a502b26de26b46ae5ef19ee174
Daniel Colson <danieljamescolson@gmail.com>
2017-02-02 14:02:38 -0500

    MovieBuff project to rails 5

--- sql/projects/movie_buff/skeleton/app/models/actor.rb
+++ sql/projects/movie_buff/skeleton/app/models/actor.rb
@@ -1,14 +1,14 @@
-class Actor < ActiveRecord::Base
-  
+class Actor < ApplicationRecord
+
   has_many :castings,
     class_name: "Casting",
     foreign_key: :actor_id,
     primary_key: :id
-  
+
   has_many :movies,
     through: :castings,
     source: :movie
-  
+
   has_many :directed_movies,
     class_name: "Movie",
     foreign_key: :director_id,
--- sql/projects/movie_buff/solution/app/models/casting.rb
+++ sql/projects/movie_buff/solution/app/models/casting.rb
@@ -1,10 +1,10 @@
-class Casting < ActiveRecord::Base
-  
+class Casting < ApplicationRecord
+
   belongs_to :actor,
     class_name: "Actor",
     foreign_key: :actor_id,
     primary_key: :id
-  
+
   belongs_to :movie,
     class_name: "Movie",
     foreign_key: :movie_id,
