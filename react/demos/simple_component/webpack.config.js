c9bfe9c37b70311cca8ba16af4f650fb9e9c5d61
Debra Fong <debsfong@gmail.com>
2017-03-10 14:52:04 -0800

    use webpack 2 in simple-component demo

--- react/demos/simple_component/webpack.config.js
+++ react/demos/simple_component/webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
       {
         test: [/\.jsx?$/, /\.js?$/],
         exclude: /node_modules/,
-        loader: 'babel',
+        loader: 'babel-loader',
         query: {
           presets: ['react', 'es2015']
         }
