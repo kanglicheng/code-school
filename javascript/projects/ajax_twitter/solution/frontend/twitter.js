35686e4316d85c620358328e20a99cf8ad357fca
Meagan Fenisey-Copes <mfeniseycopes@gmail.com>
2017-08-10 12:20:55 -0400

    Restyle user search

--- javascript/projects/ajax_twitter/solution/frontend/twitter.js
+++ javascript/projects/ajax_twitter/solution/frontend/twitter.js
@@ -6,6 +6,6 @@ const UsersSearch = require('./users_search');
 $(function () {
   $('div.infinite-tweets').each( (i, tweet) => new InfiniteTweets(tweet) );
   $('form.tweet-compose').each( (i, form) => new TweetCompose(form) );
-  $('nav.users-search').each( (i, search) => new UsersSearch(search) );
+  $('.users-search').each( (i, search) => new UsersSearch(search) );
   $('button.follow-toggle').each( (i, btn) => new FollowToggle(btn, {}) );
 });
