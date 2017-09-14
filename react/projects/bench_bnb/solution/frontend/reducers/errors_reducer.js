9a7da46e5503f34b4d49907a2d27d4544f7f0b73
Hope <hopewanroy@gmail.com>
2017-09-14 15:55:22 -0700

    add reducer for session errors in errors reducer

--- react/projects/bench_bnb/solution/frontend/reducers/errors_reducer.js
+++ react/projects/bench_bnb/solution/frontend/reducers/errors_reducer.js
@@ -1,26 +1,7 @@
-import merge from 'lodash/merge';
+import { combineReducers } from 'redux';
 
-import {
-  RECEIVE_ERRORS,
-  RECEIVE_CURRENT_USER,
-} from '../actions/session_actions';
+import session from './session_errors_reducer';
 
-const defaultState = Object.freeze({
-  session: [],
+export default combineReducers({
+  session
 });
-
-const ErrorsReducer = (state = defaultState, action) => {
-  Object.freeze(state);
-  switch(action.type) {
-    case RECEIVE_ERRORS:
-      return merge({}, defaultState, {
-        session: action.errors
-      });
-    case RECEIVE_CURRENT_USER:
-      return defaultState;
-    default:
-      return state;
-  }
-};
-
-export default ErrorsReducer;
