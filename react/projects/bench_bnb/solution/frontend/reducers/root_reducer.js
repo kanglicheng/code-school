79b79a0b3c25a773aa9acc99dafc8868ba7132f9
Hope <hopewanroy@gmail.com>
2017-09-14 15:44:29 -0700

    consistent importing of reducers

--- react/projects/bench_bnb/solution/frontend/reducers/root_reducer.js
+++ react/projects/bench_bnb/solution/frontend/reducers/root_reducer.js
@@ -2,12 +2,12 @@ import { combineReducers } from 'redux';
 
 import entities from './entities_reducer';
 import ui from './ui_reducer';
-import SessionReducer from './session_reducer';
+import session from './session_reducer';
 import errors from './errors_reducer';
 
 const RootReducer = combineReducers({
   entities,
-  session: SessionReducer,
+  session,
   ui,
   errors,
 });
