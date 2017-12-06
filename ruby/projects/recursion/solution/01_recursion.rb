29e0a42de94b189e7d9f113de7ab4582f1d76079
Aaron Wayne <amwayne90@gmail.com>
2017-12-06 09:38:29 -0800

    Remove a bad solution and comments

--- ruby/projects/recursion/solution/01_recursion.rb
+++ ruby/projects/recursion/solution/01_recursion.rb
@@ -34,8 +34,6 @@ end
 
 class Array
   def deep_dup
-    # Argh! Mario and Kriti beat me with a one line version?? Must
-    # have used `inject`...
     new_array = []
     self.each do |el|
       new_array << (el.is_a?(Array) ? el.deep_dup : el)
@@ -43,13 +41,7 @@ class Array
     new_array
   end
 
-  # The renowned one-line inject version of deep_dup
-  # Beware inject!
-  def dd_inject
-    inject([]) { |dup, el| dup << (el.is_a?(Array) ? el.dd_inject : el) }
-  end
-
-  # Beware map! The ultimate one-liner.
+  # The ultimate one-liner!
   def dd_map
     map { |el| el.is_a?(Array) ? el.dd_map : el }
   end
